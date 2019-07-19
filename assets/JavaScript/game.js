acd35edc77a3ae042566534737ad4d135187735d
NelsonCad <NelsonCad97@gmail.com>
2019-07-19 10:10:35 -0700

    generated buttons with assigned values

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,25 +1,43 @@
+$(document).ready(function(){
+
+
+
+
 //created array to spawn buttons from
-let betaKids = ["john", "jade", "dave", "rose"];
+var betaKids = ["john", "jade", "dave", "rose"];
 
 //dynamically create the buttons here
 betaKids.forEach(function (kid) {
     //creates a button for each kid
     let userBtn = $("<button>");
     
-    //let randomValue = Math.floor((Math.random() * 10) + 1);
-
+    let random = Math.floor((Math.random() * 100) + 1);
+    
+    value = random;
     //adds classes to each button created
     userBtn.addClass("kid-button kid kid-button-color");
 
     //adds the data of the kid to each button generated
-    userBtn.attr("data-letter",kid);
+    userBtn.attr("data-letter", kid);
+    userBtn.attr("data-value", value);
+    
 
     //adds the name of the kid to the front of the button generated
     userBtn.text(kid);
 
-
+    console.log(value)
 
 
     //adds all above data to the next open slot for a button
-    $("#boxen").append(userBtn + "<br>");
+    $("#boxen").append(userBtn);
+    $("#boxen").append("<br>");
 });
+
+$(".kid-button").on("click", function(){
+
+    
+})
+
+
+
+})
